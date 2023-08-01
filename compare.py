9d5113a137d735519ca1da813c68f21c1ecdb373
proxyv <140994337+proxyv@users.noreply.github.com>
2023-08-01 02:43:19 -0700

    evaluation and conditional stmts

---
+++ compare.py
@@ -0,0 +1,11 @@
+x = int(input("what's x? "))
+y = int(input("What's y? "))
+
+if x<y:
+    print ("X is less than y")
+
+elif x>y:
+    print ("x is greater than y")
+
+else:
+    print ("X is equal to y")
