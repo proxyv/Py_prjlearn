25f3ffda57d9700e8e66a4720b6c7b9a6fa6e78a
proxyv <140994337+proxyv@users.noreply.github.com>
2023-09-09 00:44:23 -0700

    sum of the current number and the previous number

---
+++ SumOfCurNprev.py
@@ -0,0 +1,10 @@
+def main():
+    #prevnum=0
+    for i in range(1,11):
+        curnum=i
+        prevnum=i-1
+        total=curnum+prevnum
+        print(f"Current Number is {curnum} Previous number is {prevnum} Sum:{total}")
+        
+
+main()
